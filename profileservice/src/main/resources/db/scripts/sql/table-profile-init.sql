b771622c8f110c1f1dc16d367b8de3fa4760a1e7
hyulevo653 <duchuy653201@gmail.com>
2025-01-02 10:34:55 +0700

    feat: add liquibase

---
+++ profileservice/src/main/resources/db/scripts/sql/table-profile-init.sql
@@ -0,0 +1,3 @@
+INSERT INTO profile (id, email, status, name, role) VALUES
+(1, 'dev@gmail.com', 'ACTIVE', 'Nguyễn Đức Huy', 'DEVELOPER'),
+(2, 'customer@gmail.com', 'ACTIVE', 'Khach hang', 'CUSTOMER');
