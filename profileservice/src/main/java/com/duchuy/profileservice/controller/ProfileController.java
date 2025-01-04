1e74c9edf09267bf24ad1b2dd09df65e25209535
hyulevo653 <duchuy653201@gmail.com>
2025-01-04 13:51:41 +0700

    update : json schema validate profileService

--- profileservice/src/main/java/com/duchuy/profileservice/controller/ProfileController.java
+++ profileservice/src/main/java/com/duchuy/profileservice/controller/ProfileController.java
@@ -1,7 +1,10 @@
 package com.duchuy.profileservice.controller;
 
+import com.duchuy.commonservice.utils.CommonFunction;
 import com.duchuy.profileservice.model.ProfileDTO;
 import com.duchuy.profileservice.service.ProfileService;
+import com.duchuy.profileservice.utils.Constant;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +12,14 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.InputStream;
+
 @RestController
 @RequestMapping("/api/v1/profiles")
 public class ProfileController {
     @Autowired
     ProfileService profileService;
+    Gson gson = new Gson();
 
     @GetMapping
     public ResponseEntity<Flux<ProfileDTO>> getAllProfile(){
@@ -24,7 +30,9 @@ public class ProfileController {
         return ResponseEntity.ok(profileService.checkDuplicate(email));
     }
     @PostMapping
-    public ResponseEntity<Mono<ProfileDTO>> createNewProfile(@RequestBody ProfileDTO profileDTO){
-        return ResponseEntity.status(HttpStatus.CREATED).body(profileService.createNewProfile(profileDTO));
+    public ResponseEntity<Mono<ProfileDTO>> createNewProfile(@RequestBody String requestStr){
+        InputStream inputStream = ProfileController.class.getClassLoader().getResourceAsStream(Constant.JSON_REQ_CREATE_PROFILE);
+        CommonFunction.jsonValidate(inputStream,requestStr);
+        return ResponseEntity.status(HttpStatus.CREATED).body(profileService.createNewProfile(gson.fromJson(requestStr,ProfileDTO.class)));
     }
 }
