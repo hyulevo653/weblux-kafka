69d4acb05f96b9260cc480b7043ffaa73ede23b5
hyulevo653 <duchuy653201@gmail.com>
2025-01-13 15:23:05 +0700

    feat: add crud account service

---
+++ accountservice/src/main/java/com/duchuy/accountservice/controller/AccountController.java
@@ -0,0 +1,60 @@
+package com.duchuy.accountservice.controller;
+
+
+import com.duchuy.accountservice.model.AccountDTO;
+import com.duchuy.accountservice.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/account")
+public class AccountController {
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping
+    public Flux<AccountDTO> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<AccountDTO> createAccount(@RequestBody AccountDTO request){
+        return accountService.createAccount(request);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Map<String, Object>>> updateAccount(@PathVariable String id, @RequestBody AccountDTO accountDTO) {
+        return accountService.updateAccount(id, accountDTO)
+                .map(updatedAccount -> {
+                    Map<String, Object> response = new HashMap<>();
+                    response.put("status", 200);
+                    response.put("message", "Cập nhật thành công");
+                    response.put("data", AccountDTO.entityToModel(updatedAccount)); // Trả về dữ liệu đã cập nhật
+                    return ResponseEntity.ok(response);
+                })
+                .defaultIfEmpty(ResponseEntity.notFound().build()); // Nếu không tìm thấy tài khoản, trả về 404
+    }
+
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Map<String, Object>>> deleteAccount(@PathVariable String id) {
+        return accountService.deleteAccount(id)
+                .then(Mono.just(ResponseEntity.ok().body(createResponse(200, "Xóa thành công"))))
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.NOT_FOUND).body(createResponse(404, "Tài khoản không tồn tại")));
+    }
+    private Map<String, Object> createResponse(int status, String message) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", status);
+        response.put("message", message);
+        return response;
+    }
+}
--- accountservice/src/main/java/com/duchuy/accountservice/service/AccountService.java
+++ accountservice/src/main/java/com/duchuy/accountservice/service/AccountService.java
@@ -1,10 +1,12 @@
 package com.duchuy.accountservice.service;
 
+import com.duchuy.accountservice.data.Account;
 import com.duchuy.accountservice.model.AccountDTO;
 import com.duchuy.accountservice.repository.AccountRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -19,4 +21,33 @@ public class AccountService {
                 .map(AccountDTO::entityToModel)
                 .doOnError(throwable -> log.error(throwable.getMessage()));
     }
+    public Flux<AccountDTO> getAllAccounts() {
+        return accountRepository.findAll()
+                .map(AccountDTO::entityToModel);
+    }
+
+    public Mono<AccountDTO> createAccount(AccountDTO accountDTO){
+        Account account = AccountDTO.dtoToEntity(accountDTO);
+        return accountRepository.save(account)
+                .map(AccountDTO::entityToModel);
+    }
+
+    public Mono<Account> updateAccount(String id, AccountDTO accountDTO) {
+        return accountRepository.findById(id)
+                .flatMap(existingAccount -> {
+                    existingAccount.setEmail(accountDTO.getEmail());
+                    existingAccount.setCurrency(accountDTO.getCurrency());
+                    existingAccount.setBalance(accountDTO.getBalance());
+                    existingAccount.setReserved(accountDTO.getReserved());
+
+                    return accountRepository.save(existingAccount);
+                });
+    }
+
+    public Mono<Void> deleteAccount(String id) {
+        return accountRepository.findById(id)
+                .flatMap(account -> accountRepository.delete(account)) // Xóa tài khoản
+                .then(); // Trả về Mono<Void> khi xóa thành công
+    }
+
 }
